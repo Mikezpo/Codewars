320cc80f31224a0daf9417b610f08456f7ae196a
jose oliveira <74796059+Mikezpo@users.noreply.github.com>
2023-02-07 16:02:32 +0000

    Codewars challenge added
    In this challenge I had to find a solution to why the code was not executing properly.

---
+++ Code JS 2/main.js
@@ -0,0 +1,22 @@
+// Multiply
+
+// This code does not execute properly. Try to figure out why.
+
+// function multiply(a, b){
+//   a * b
+// }
+
+
+multiply = function (a, b) {
+    return a * b;
+  }
+
+// ||
+
+multiply = (a, b) => a * b;
+
+// ||
+
+function multiply(a, b){
+    return a * b
+  }
