5701e135e479813fb18fe376cdb3fef9235879c7
jose oliveira <74796059+Mikezpo@users.noreply.github.com>
2023-03-14 23:48:13 +0000

    Challenge added
    Double the integer and return it.

---
+++ Code JS 27/main.js
@@ -0,0 +1,18 @@
+// You Can't Code Under Pressure #1
+
+// Code as fast as you can! You need to double the integer and return it.
+
+
+function doubleInteger(i) {
+    return i * 2;
+}
+
+// ||
+
+const doubleInteger = i => 2 * i;
+
+// ||
+
+function doubleInteger(i) {
+    return i << 1;
+}
