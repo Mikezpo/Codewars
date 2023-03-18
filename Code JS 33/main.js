cc198046501de6c3b86cf7938f86f309ebfa38e9
jose oliveira <74796059+Mikezpo@users.noreply.github.com>
2023-03-18 18:32:25 +0000

    Challenge added
    Write a function which converts the input string to uppercase.

---
+++ Code JS 33/main.js
@@ -0,0 +1,15 @@
+// MakeUpperCase
+
+// Write a function which converts the input string to uppercase.
+
+function makeUpperCase(str) {
+    return str.toUpperCase();
+}
+
+// ||
+
+const makeUpperCase = str => str.toUpperCase();
+
+// ||
+
+function makeUpperCase(str) {return str.toUpperCase();}
