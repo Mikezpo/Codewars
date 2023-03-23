e42d78ab51fc58a613eed83369e4c7a7399a25df
jose oliveira <74796059+Mikezpo@users.noreply.github.com>
2023-03-23 12:40:49 +0000

    Challenge added
    Create a function that returns an a string depending on the initial letter of the parameter.

---
+++ Code JS 37/main.js
@@ -0,0 +1,33 @@
+// Are You Playing Banjo?
+
+// Create a function which answers the question "Are you playing banjo?".
+// If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+// The function takes a name as its only argument, and returns one of the following strings:
+
+// name + " plays banjo" 
+// name + " does not play banjo"
+
+// Names given are always valid strings.
+
+
+function areYouPlayingBanjo(name) {
+    let anotherLetter = name.split('')
+    if (anotherLetter[0].toLowerCase() !== 'r') {
+          return `${name} does not play banjo`;
+      } else {
+          return `${name} plays banjo`;
+    }
+}
+
+// ||
+
+function areYouPlayingBanjo(name) {
+    return name + (name[0].toLowerCase() == 'r' ? ' plays' : ' does not play') + " banjo";
+}
+
+// ||
+
+function areYouPlayingBanjo(name) {
+    return name + (/^r/i.test(name) ? " plays " : " does not play ") + "banjo";
+}
