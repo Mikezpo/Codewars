89f988c8e27557fbf7ecb7978d881f065ba5bf07
jose oliveira <74796059+Mikezpo@users.noreply.github.com>
2023-03-25 20:28:55 +0000

    Challenge added
    Return the result of multiplying the values of a given array.

---
+++ Code JS 42/main.js
@@ -0,0 +1,24 @@
+// Beginner - Reduce but Grow
+
+// Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+// [1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24
+
+
+function grow(x){
+    let result = 1;
+    for (let i = 0; i < x.length; i++) {
+      result *= x[i];
+    }
+    return result;
+}
+
+// ||
+
+const grow=x=> x.reduce((a,b) => a*b);
+
+// ||
+
+function grow(x){
+    return x.reduce((a, b)=> a * b,1);
+}
