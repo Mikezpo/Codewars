67d2971235f3acd68cc2edf11ad383dc4d76765c
jose oliveira <74796059+Mikezpo@users.noreply.github.com>
2023-03-27 21:00:10 +0100

    Challenge added
    Write a function that returns the BMI index.

---
+++ Code JS 46/main.js
@@ -0,0 +1,49 @@
+// Calculate BMI
+
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+
+
+function bmi(w, h) {
+    let body = w / (h * h);
+    if (body <= 18.5) {
+      return 'Underweight';
+    }else if (body <= 25.0) {
+      return 'Normal';
+    }else if (body <= 30.0) {
+      return 'Overweight';
+    }else {
+      return 'Obese';
+    }
+}
+
+// ||
+
+function bmi(weight, height) {
+    var result = weight/Math.pow(height,2) 
+    
+    if (result <= 18.5) {
+      return "Underweight";
+    } else if (result <= 25) {
+      return "Normal";
+    } else if (result <= 30) {
+      return "Overweight";
+    } else {
+      return "Obese";
+    }
+    
+}
+
+// ||
+
+const bmi = (w, h, bmi = w/h/h) =>  bmi <= 18.5 ? "Underweight" :
+                                    bmi <= 25 ? "Normal" :
+                                    bmi <= 30 ? "Overweight" : "Obese";
