0310bbb38451acb45c6813665f722b64f5b3df51
jose oliveira <74796059+Mikezpo@users.noreply.github.com>
2023-03-31 23:05:29 +0100

    Challenge added
    In a given array of words return them into a sentence.

---
+++ Code JS 51/main.js
@@ -0,0 +1,30 @@
+// Sentence Smash
+
+// Sentence Smash
+
+// Write a function that takes an array of words and smashes them together into a sentence and returns the sentence. You can ignore any need to sanitize words or add punctuation, but you should add spaces between each word. Be careful, there shouldn't be a space at the beginning or the end of the sentence!
+// Example
+
+// ['hello', 'world', 'this', 'is', 'great']  =>  'hello world this is great'
+
+
+
+
+function smash (words) {
+  
+    let sentence = words.join(' ');
+     return sentence;
+};
+
+// ||
+
+smash = function (words) {
+    return words.join(" ");
+};
+
+// ||
+
+function smash (words) {
+    "use strict";
+    return words.join(' ');
+};
