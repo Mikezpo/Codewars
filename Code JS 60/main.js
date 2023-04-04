b9e2feeb02ff646161bb2c2340333067ddd30156
jose oliveira <74796059+Mikezpo@users.noreply.github.com>
2023-04-05 00:01:25 +0100

    Challenge added
    Remove exclamation marks on a given string.

---
+++ Code JS 60/main.js
@@ -0,0 +1,20 @@
+// Remove exclamation marks
+
+// Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+
+function removeExclamationMarks(s) {
+    return s.replaceAll('!', '');
+}
+
+// ||
+
+function removeExclamationMarks(s) {
+    return s.replace(/!/gi, '');
+}
+
+// ||
+
+function removeExclamationMarks(s) {
+    return s.split('!').join('');
+}
