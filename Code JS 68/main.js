a38a878efad2aa6d0b82a32ddc267fff603bf988
jose oliveira <74796059+Mikezpo@users.noreply.github.com>
2023-04-07 16:20:38 +0100

    Challenge added
    In a given string, return it with double characters.

---
+++ Code JS 68/main.js
@@ -0,0 +1,29 @@
+// Double Char
+
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+// Examples (Input -> Output):
+
+// * "String"      -> "SSttrriinngg"
+// * "Hello World" -> "HHeelllloo  WWoorrlldd"
+// * "1234!_ "     -> "11223344!!__  "
+
+// Good Luck!
+
+
+function doubleChar(str) {
+    let double = '';
+    for (let i = 0; i < str.length; i++) {
+      double = double + (str[i] + str[i]);
+  }
+    return double;
+}
+
+// ||
+
+const doubleChar = (str) => str.split("").map(c => c + c).join("");
+
+// ||
+
+function doubleChar(str) {
+    return str.replace(/(.)/g, "$1$1")
+}
