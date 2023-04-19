47e60b2075b8eccc171e3761d7b9f4a37c6a0c6e
jose oliveira <74796059+Mikezpo@users.noreply.github.com>
2023-04-19 11:31:45 +0100

    Challenge added
    Reversed words.

---
+++ Code JS 83/main.js
@@ -0,0 +1,25 @@
+// Reversed Words
+
+// Complete the solution so that it reverses all of the words within the string passed in.
+
+// Words are separated by exactly one space and there are no leading or trailing spaces.
+
+// Example(Input --> Output):
+
+// "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
+
+
+function reverseWords(str){
+    let reversed = str.split(' ').reverse().join(' ')
+    return reversed;
+}
+
+// ||
+
+function reverseWords(str){
+    return str.split(' ').reverse().join(' ');
+}
+
+// ||
+
+const reverseWords = (str) => str.split(" ").reverse().join(" ");
